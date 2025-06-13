6d1e9b85400c8da9833830aa75dbc5633fbbe906
sjlee-catthe <ee395000@gmail.com>
2025-06-13 15:34:23 +0900

    commit

---
+++ frontend/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+        <VerticalNavSectionTitle :item="{ heading: 'Order' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Order',
+                to: '/orders',
+            }"
+        />
+        <VerticalNavSectionTitle :item="{ heading: 'Inventory' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Inventory',
+                to: '/inventories',
+            }"
+        />
+        <VerticalNavSectionTitle :item="{ heading: 'Delivery' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Delivery',
+                to: '/deliveries',
+            }"
+        />
+    </ul>
+</template>
